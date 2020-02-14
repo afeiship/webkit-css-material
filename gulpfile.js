3850c4d6582c3accf016f176a50c759db6da150e
afeiship <1290657123@qq.com>
2020-02-14 14:38:52 +0800

    feat: update usable version

--- gulpfile.js
+++ gulpfile.js
@@ -4,34 +4,23 @@
  *  because putting all here was really too long
  */
 
-'use strict';
+"use strict";
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var del = require('del');
-var autoprefixer = require('gulp-autoprefixer');
-var debug = require('gulp-debug');
-var rename = require('gulp-rename');
-var concat = require('gulp-concat');
-var debug = require('gulp-debug');
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var del = require("del");
 
-gulp.task('clean', function () {
-  return del('dist');
+gulp.task("clean", function() {
+  return del("dist");
 });
 
-gulp.task('sass', function () {
-  return gulp.src('src/*.scss')
-    .pipe(gulp.dest('dist'))
-    .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
-    .pipe(gulp.dest('dist'))
-    .pipe(gulp.dest('docs'))
-    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-    .pipe(rename({ extname:'.min.css' }))
-    .pipe(gulp.dest('docs'))
-    .pipe(gulp.dest('dist'));
+gulp.task("styles", function() {
+  return gulp
+    .src("src/*.scss")
+    .pipe(gulp.dest("dist"))
+    .pipe(sass({ outputStyle: "expanded" }).on("error", sass.logError))
+    .pipe(gulp.dest("dist"))
+    .pipe(gulp.dest("docs"));
 });
 
-
-gulp.task('default', ['clean'], function () {
-  gulp.start(['sass']);
-});
+gulp.task("default", gulp.series(["clean", "styles"]));
